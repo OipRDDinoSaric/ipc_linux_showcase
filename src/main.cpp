c04e96f3745a01a0750256f27f79429ef33894a2
OipRDDinoSaric <account@oip-rd.com>
2024-09-15 19:33:21 +0200

    Add all threads

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
-#include <fmt/ostream.h>
+#include <thread>
+#include <random>
+#include <chrono>
+
+#include <fmt/format.h>
+
+namespace Producer
+{
+void
+generateTask()
+{}
+
+void
+acknowledgeReceiveTask()
+{}
+}  // namespace Producer
+
+namespace Consumer
+{
+void
+receiveTask()
+{}
+
+void
+acknowledgeSendTask()
+{}
+}  // namespace Consumer
 
 int
-main(int, char* [])
+main(int, char*[])
 {
+    // Note that jthread is not supported by clang I used.
+    std::thread generatorThread {Producer::generateTask};
+    std::thread acknowledgeReceiveThread {Producer::acknowledgeReceiveTask};
 
+    std::thread receiveThread {Consumer::receiveTask};
+    std::thread acknowledgeSendThread {Consumer::acknowledgeSendTask};
 
-    return EXIT_SUCCESS;
+    generatorThread.join();
+    acknowledgeReceiveThread.join();
+
+    receiveThread.join();
+    acknowledgeSendThread.join();
+
+    return 0;
 }
