d2a238c71ed55148d8013d71ec8dc83cf644f527
OipRDDinoSaric <account@oip-rd.com>
2024-09-15 19:33:53 +0200

    Add RNG ready for sending

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,24 @@ namespace Producer
 {
 void
 generateTask()
-{}
+{
+    constexpr static std::mt19937::result_type lowerBound {0};
+    constexpr static std::mt19937::result_type upperBound {100};
+
+    std::random_device device {};
+    std::mt19937 rng {device()};
+    std::uniform_int_distribution<std::mt19937::result_type> distribution {lowerBound, upperBound};
+
+    for(int iii {0}; iii < 10; iii++)
+    {
+        using std::chrono_literals::operator""ms;
+
+        std::mt19937::result_type generatedNumber {distribution(rng)};
+        fmt::println("Generated number {}: {}", iii, generatedNumber);
+
+        std::this_thread::sleep_for(500ms);
+    }
+}
 
 void
 acknowledgeReceiveTask()
