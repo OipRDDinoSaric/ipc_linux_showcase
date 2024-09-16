6c361f28eddce95dd52d2a0be264bde8a35a96bb
OipRDDinoSaric <account@oip-rd.com>
2024-09-16 09:08:32 +0200

    Move executable to the project base

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 
+#include <consume/Consumer.hpp>
 #include <produce/Producer.hpp>
 
 #include <fmt/format.h>
@@ -7,6 +8,17 @@
 int
 main(int, char*[])
 {
+    fmt::println("Consumer started.");
+
+    // TODO fork()
+
+    // Note that jthread is not supported by clang I used.
+    std::thread receiveThread {Consume::Consumer::receiveTask};
+    std::thread acknowledgeSendThread {Consume::Consumer::acknowledgeSendTask};
+
+    receiveThread.join();
+    acknowledgeSendThread.join();
+
     fmt::println("Producer started.");
 
     // Note that jthread is not supported by clang I used.
